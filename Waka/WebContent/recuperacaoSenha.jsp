782590999e5df45ab97f2ee9974a76915ffc26c4
Italo <Italo@192.168.0.139>
2017-11-01 11:33:09 -0200

    Recuperacao de Senha com Email

--- Waka/WebContent/recuperacaoSenha.jsp
+++ Waka/WebContent/recuperacaoSenha.jsp
@@ -3,18 +3,34 @@
 
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">>
+	<meta charset="utf-8">
 <title>Recuperação de Senha</title>
 </head>
 <body>
 <h1>Recuperação de Senha</h1> 
-<form action="" method="post">
+<form action="RecuperarSenhaServlet" method="get">
+<!-- RETORNO DO VALIDAR EMAIL(MENSAGEM DE ERRO) -->
+			<%if(request.getAttribute("validarEmail")== null) 
+			request.setAttribute("validarEmail", "");
+			%>
 
 		<table>
 			<tr>
-				<td>Email:</td>
-				<td><input type="email" required="required" id="email" name="email" ></td>
-			    <td><input type="submit" value="Enviar Solicitação" ></tr>
+				<td>Email:
+				<input type="email" required="required" id="email" name="email" ></td>
+				
+			</tr>
+				<!-- VALIDAR EMAIL -->
+				<center style="color: red";> <%=request.getAttribute("validarEmail") %></center>
+					
+				 <tr>
+				 <td><p>Selecione Usuario: <select name="tipoLogin" required="required"></p>
+					<option value="Empreendedor">Empreendedor</option>
+					<option value="Investidor">Investidor</option></td>
+					</tr>
+				<tr>
+			    <td><input type="submit" value="Enviar Solicitação" > </td>
+			    </tr>
 			</table>
 	</form>
 </body>
--- Waka/src/model/DaoEmpreendedor.java
+++ Waka/src/model/DaoEmpreendedor.java
@@ -1,25 +1,28 @@
 package model;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.Random;
+import Utilitarios.*;
 import connection.SingleConnection;
 
+
 public class DaoEmpreendedor {
-	
-	
+
 	private Connection connection;
 
-	 public DaoEmpreendedor() {
+	public DaoEmpreendedor() {
 		// CONSTRUTOR:
-		 connection= SingleConnection.getConection();
+		connection = SingleConnection.getConection();
 	}
-	
-	 public void cadastroEmpreendedor(BeanEmpreendedor empreendedor){
-		 try {
-			PreparedStatement stmt = connection.prepareStatement("insert into empreendedor(nome, dataNascimento, telefone, endereco, sexo, email, senha) value(?,?,?,?,?,?,?)");
+
+	public void cadastroEmpreendedor(BeanEmpreendedor empreendedor) {
+		try {
+			PreparedStatement stmt = connection.prepareStatement(
+					"insert into empreendedor(nome, dataNascimento, telefone, endereco, sexo, email, senha) value(?,?,?,?,?,?,?)");
 			stmt.setString(1, empreendedor.getNome());
 			stmt.setString(2, empreendedor.getDataNascimento());
 			stmt.setString(3, empreendedor.getTelefone());
@@ -27,28 +30,130 @@ public class DaoEmpreendedor {
 			stmt.setString(5, empreendedor.getSexo());
 			stmt.setString(6, empreendedor.getEmail());
 			stmt.setString(7, empreendedor.getSenha());
-			
+
 			stmt.execute();
 			System.out.println("Passou em EMPREENDEDOR DAO!");
 			connection.commit();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			try {
 				connection.rollback();
 			} catch (SQLException e1) {
-				
+
 				e1.printStackTrace();
 			}
 		}
-		 
+
+	}
+
+	public boolean validarEmail(String usuaioLogado) throws Exception {
+		PreparedStatement stmt = connection.prepareStatement("select * from empreendedor where email=?");
+		stmt.setString(1, usuaioLogado);
+		ResultSet rs = stmt.executeQuery();
+		System.out.println("Passou EM VALIADAR EMAIL:");
+		if (rs.next()) {
+			// DEU CERTO:
+			System.out.println("EMAIL VALIDO");
+			return true;
+
+		} else {
+			// DEU ERRO:
+			System.out.println("EMAIL INVALIDO");
+			return false;
+
+		}
+
+	}
+
+	public int codigoSeguraca() {
+		Random radom = new Random();
+		int numeroTmp = 0;
+		for (int i = 0; i < 10; i++) {
+			numeroTmp = radom.nextInt(20000);
+			System.out.println(">" + numeroTmp);
+		}
+		return numeroTmp;
+
+	}
+
+	
+	//INSERIR O CODIGO NO BANCO DE DADOS
+		public void gravarCodigo(String usuarioLogado){
+			
+			int numeroGerado = codigoSeguraca();
+			try {
+				PreparedStatement stmt = connection.prepareStatement("insert into seguranca(codigo, email) value(?,?)");
+				stmt.setInt(1, numeroGerado);
+				stmt.setString(2, usuarioLogado);
+				stmt.execute();
+				connection.commit();
+				
+				
+				System.out.println("passou em GRAVAR CODIGO");
+				
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				//MANDAR NUMERO GERADO PARA SERVLET
+				
+				
+			}
+			System.out.println("VALOR FINAL DO CODIGO DO USUARIO  :"+numeroGerado);
+			//ENVIAR EMAIL
+	enviarEmail.enviarGmail(usuarioLogado, numeroGerado);
 		
-	 }
-	 
-	 
+		}
+		
+		
+		
+		//VERIFICAR SE O CODIGO PERTENCE AO USUARIO
+		public boolean validarCodigo(int codigoInformado)  throws Exception{
+				
+				PreparedStatement stmt = connection.prepareStatement("select * from seguranca where codigo=?");
+				stmt.setInt(1, codigoInformado);
+				ResultSet rs = stmt.executeQuery();
+				
+				if(rs.next()){
+					// DEU CERTO:
+					return true;
+				}else{
+					// DEU ERRO:
+					return false;
+				}
+				
+			}
+
+
+		
+		public void salvarNovaSenha(String usuarioLogado, String novaSenha){
+			   System.out.println("Nova seha que chegou aqui "+novaSenha);
+			   try {
+				PreparedStatement stmt = connection.prepareStatement("update empreendedor set senha = ? where email = ?");
+				stmt.setString(1, novaSenha);
+				stmt.setString(2, usuarioLogado);
+				stmt.execute();
+				System.out.println("passou aqui no dao nova senha");
+				connection.commit();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			   
+		   }
+		   
 	
-	 
-	
-	 
 	
 }
